defe570309b8207d4ced25786857343dc696e647
Lasana Murray <lmurray@quenk.com>
2018-12-08 17:06:11 -0400

    refactor(cli/command): update regex

--- lib/cli/command/get.ts
+++ lib/cli/command/get.ts
@@ -8,7 +8,7 @@ import {
 } from '@quenk/noni/lib/io/file';
 import { endsWith } from '@quenk/noni/lib/data/string';
 import { noop } from '@quenk/noni/lib/data/function';
-import { Future, parallel, liftP  } from '@quenk/noni/lib/control/monad/future';
+import { Future, parallel, liftP } from '@quenk/noni/lib/control/monad/future';
 import { polate } from '@quenk/polate';
 import { Context, Options } from '../options';
 
@@ -17,8 +17,8 @@ export const EXT_ERIC = '.eric';
 const polateOptions = {
 
     start: '{{{',
-
-    end: '}}}'
+    end: '}}}',
+    regex: '(.+)'
 
 }
 
